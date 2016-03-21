05215e35a7c0e62f276be0a72d2f1745283dac40
Amanda Buhr <amandabuhr@Amandas-MacBook-Pro.local>
2016-03-21 15:12:40 -0400

    Added class to test main method

--- app/src/androidTest/java/scottnickamanda/sliderpuzzle/MainActivityTest.java
+++ app/src/androidTest/java/scottnickamanda/sliderpuzzle/MainActivityTest.java
@@ -1,30 +1,28 @@
 package scottnickamanda.sliderpuzzle;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Tests MainActivity
  *
  * Created by Amanda Buhr on 3/19/16.
  */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
 
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    @Rule public final ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
 
-    MainActivity activity;
-
-    public MainActivityTest() {
-        super(MainActivity.class);
-    }
-
-
-    @Override
-    protected void setUp() throws Exception {
-
-        super.setUp();
-        activity = getActivity();
-    }
-
+    @Test
     public void testActivityExists() {
-        assertNotNull(activity);
+        onView(withId(R.id.selectImage)).check(matches(isClickable()));
     }
 }
